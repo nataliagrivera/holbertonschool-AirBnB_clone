b0b95cf6e4e7cc4b4b087075486c1c28c902c3eb
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-31 13:19:37 -0400

    updated base

--- models/base_model.py
+++ models/base_model.py
@@ -19,10 +19,11 @@ class BaseModel:
             create_at: datetime when the object is created
             updated_at: datetime when the object is updated"""
         for k, v in kwargs.items():
-            if k in ['created_at', 'updated_at']:
-                setattr(self, k, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
-            else:
-                setattr(self, k, v)
+            if k != '__class__':
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, k, v)
 
         else:
             # If no keyword arguments are provided, generate new values
