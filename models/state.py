13b4cd16ad36b0c886a862cfe98d98adec81d31a
jnieves2177 <jnieves3177@gmail.com>
2023-11-01 19:44:41 +0000

    models

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines the State class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Inherits from BaseModel
+    Public class attribute:
+        name: (str)
+    """
+    name = ""
