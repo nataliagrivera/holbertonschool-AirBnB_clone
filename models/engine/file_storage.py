40a3b82450487792425f2e8c81296027914cdd45
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-01 09:51:14 -0400

    fixing base and storage. SLight changes.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""File Storage"""
 import json
-import os.path
 import os
-
+"""
+Contains a class File Storage
+which serializes instances to a JSON file
+and deserializes JSON file to instances
+"""
 
 class FileStorage:
     _file_path = "file.json"
@@ -12,22 +14,26 @@ class FileStorage:
     def all(self):
         """Returns the dictionary __objects"""
         return FileStorage._objects
-    
+
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        name = obj.__class__.__name__ + '.' + obj.id
+        name = f"{obj.__class__.__name__}.{obj.id}"
         self._objects[name] = obj
-        
+
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         serialized_objects = {}
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in self._objects.items():
             serialized_objects[key] = obj.to_dict()
-            with open(self._file_path, 'w') as f:
-                json.dump({k: v.to_dict() for k, v in self._objects.items()}, f)
+        with open(self._file_path, 'w') as f:
+            json.dump(serialized_objects, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists"""
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)"""
         if os.path.isfile(self._file_path):
             with open(self._file_path, 'r') as f:
-                self._objects = {k: self._classes()[v['__class__']](**v) for k, v in json.load(f).items()}
+                data = json.load(f)
+                for key, obj_dict in data.items():
+                    class_name, obj_id = key.split(".")
+                    obj_dict['__class__'] = class_name
+                    self._objects[key] = eval(class_name)(**obj_dict)
