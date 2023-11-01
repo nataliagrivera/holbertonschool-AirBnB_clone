8e63f585ea4604536824a7cc883f35d445039880
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-01 09:42:56 -0400

    fixing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,24 +2,32 @@
 """File Storage"""
 import json
 import os.path
+import os
 
 
 class FileStorage:
-    def __init__(self):
-        self._file_path = "file.json"
-        self._objects = {}
+    _file_path = "file.json"
+    _objects = {}
 
     def all(self):
-        return self._objects
+        """Returns the dictionary __objects"""
+        return FileStorage._objects
     
     def new(self, obj):
-        self._objects[obj.__class__.__name__ + '.' + obj.id] = obj
-
+        """Sets in __objects the obj with key <obj class name>.id"""
+        name = obj.__class__.__name__ + '.' + obj.id
+        self._objects[name] = obj
+        
     def save(self):
-        with open(self._file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in self._objects.items()}, f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+            with open(self._file_path, 'w') as f:
+                json.dump({k: v.to_dict() for k, v in self._objects.items()}, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists"""
         if os.path.isfile(self._file_path):
             with open(self._file_path, 'r') as f:
                 self._objects = {k: self._classes()[v['__class__']](**v) for k, v in json.load(f).items()}
