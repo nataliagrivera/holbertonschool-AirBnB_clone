6497beab8f9d3f89f843d611299fe8d32b0d8328
jnieves2177 <jnieves3177@gmail.com>
2023-11-03 06:06:29 +0000

    test docstrings

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,15 +8,24 @@ from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
+    """
+    Test case class for the City model.
+    """
 
     @classmethod
     def setUpClass(cls):
+        """
+        Set up class method to create a City instance and set initial attributes.
+        """
         cls.city1 = City()
         cls.city1.name = "Anchorage"
         cls.city1.state_id = "AK"
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Tear down class method to remove test instance and cleanup resources.
+        """
         del cls.city1
         try:
             os.remove("file.json")
@@ -32,12 +41,21 @@ class TestCity(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_is_subclass(self):
+        """
+        Test if City is a subclass of BaseModel.
+        """
         self.assertTrue(issubclass(self.city1.__class__, BaseModel), True)
 
     def test_checking_for_functions(self):
+        """
+        Test if City class has a docstring.
+        """
         self.assertIsNotNone(City.__doc__)
 
     def test_has_attributes(self):
+        """
+        Test if City instance has the required attributes.
+        """
         self.assertTrue('id' in self.city1.__dict__)
         self.assertTrue('created_at' in self.city1.__dict__)
         self.assertTrue('updated_at' in self.city1.__dict__)
@@ -45,14 +63,23 @@ class TestCity(unittest.TestCase):
         self.assertTrue('name' in self.city1.__dict__)
 
     def test_attributes_are_strings(self):
+        """
+        Test if the 'name' and 'state_id' attributes are of type str.
+        """
         self.assertEqual(type(self.city1.name), str)
         self.assertEqual(type(self.city1.state_id), str)
 
     def test_save(self):
+        """
+        Test if the 'save' method updates the 'updated_at' attribute.
+        """
         self.city1.save()
         self.assertNotEqual(self.city1.created_at, self.city1.updated_at)
 
     def test_to_dict(self):
+        """
+        Test if 'to_dict' method is present in the City instance.
+        """
         self.assertEqual('to_dict' in dir(self.city1), True)
 
 
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,14 +8,23 @@ from models.base_model import BaseModel
 
 
 class TestState(unittest.TestCase):
+    """
+    This class contains unit tests for the State class and its functionalities.
+    """
 
     @classmethod
     def setUpClass(cls):
+        """
+        Set up the test class by creating an instance of the State class and initializing some attributes.
+        """
         cls.state1 = State()
         cls.state1.name = "Alaska"
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Clean up after the test class by deleting the State instance and removing the JSON data file if it exists.
+        """
         del cls.state1
         try:
             os.remove("file.json")
@@ -31,25 +40,43 @@ class TestState(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_is_subclass(self):
+        """
+        Check if the State class is a subclass of the BaseModel class.
+        """
         self.assertTrue(issubclass(self.state1.__class__, BaseModel), True)
 
     def test_checking_for_functions(self):
+        """
+        Check if the State class has a docstring.
+        """
         self.assertIsNotNone(State.__doc__)
 
     def test_has_attributes(self):
+        """
+        Check if the State instance has the expected attributes.
+        """
         self.assertTrue('id' in self.state1.__dict__)
         self.assertTrue('created_at' in self.state1.__dict__)
         self.assertTrue('updated_at' in self.state1.__dict__)
         self.assertTrue('name' in self.state1.__dict__)
 
     def test_attributes_are_strings(self):
+        """
+        Verify that the 'name'attribute of the State instance is a string.
+        """
         self.assertEqual(type(self.state1.name), str)
 
     def test_save(self):
+        """
+        Test the 'save' method to ensure that 'create_at' and 'updated_at' attributes are not equal after saving.
+        """
         self.state1.save()
         self.assertNotEqual(self.state1.created_at, self.state1.updated_at)
 
     def test_to_dict(self):
+        """
+        Check if the 'to_dict' method is defined for the State instance.
+        """
         self.assertEqual('to_dict' in dir(self.state1), True)
 
 
